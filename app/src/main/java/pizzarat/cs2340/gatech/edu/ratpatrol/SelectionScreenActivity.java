65127e68a49e2a4e8ec6c4a6edc87af0927a958d
Harrison Banh <harrisonbanh@gmail.com>
2017-09-27 15:25:19 -0400

    Added commits for javadocs requirement in M5

--- app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/SelectionScreenActivity.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/SelectionScreenActivity.java
@@ -9,10 +9,16 @@ import android.view.View;
  * This is the selection screen after logging in which allow the
  * user to switch to different screens with different functions
  * using the displayed buttons.
+ *
+ * @author Harrison Banh
  */
 public class SelectionScreenActivity extends AppCompatActivity {
     private View logoutButton;
 
+    /**
+     * Creates the SelectionScreenActivity
+     * @param savedInstanceState the bundle from the last activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +34,27 @@ public class SelectionScreenActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Switches to the LoginActivity from the SelectionScreenActivity
+     * @param view the widget that triggers the switch
+     */
     public void switchBackToLoginActivity(View view) {
         Intent startSelectionScreenActivity = new Intent(SelectionScreenActivity.this, LoginActivity.class);
         SelectionScreenActivity.this.startActivity(startSelectionScreenActivity);
     }
 
-    public void swithBackToWelcomeActivity(View view) {
+    /**
+     * Switches to the WelcomeActivity from the SelectionScreenActivity
+     * @param view the widget that triggers the switch
+     */
+    public void switchBackToWelcomeActivity(View view) {
         Intent switchToWelcomeActivity = new Intent(SelectionScreenActivity.this, Welcome.class);
         SelectionScreenActivity.this.startActivity(switchToWelcomeActivity);
     }
 
+    /**
+     * Closes the SelectionScreenActivity thus "logging out" the user
+     */
     // Make change later
     public void logout() {
        finish();

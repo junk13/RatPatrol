1f40c2ce0b9b15df7c854434fcb29317051fa7c3
Multidream <luka.derado@gmail.com>
2017-10-13 23:55:57 -0400

    Added global broker variable into Report Holder, and readme comment.

--- app/src/main/java/pizzarat/cs2340/gatech/edu/structure/ReportHolder.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/structure/ReportHolder.java
@@ -1,9 +1,32 @@
 package pizzarat.cs2340.gatech.edu.structure;
 
+import pizzarat.cs2340.gatech.edu.sqlite.SQLiteReportBroker;
+
 /**
  * Created by Luka on 10/9/2017.
  */
 
 public class ReportHolder {
     public static ReportStructure data;
+    public static SQLiteReportBroker globalBroker = new SQLiteReportBroker();
 }
+
+//TODO public void populate(){}
+//TODO public void add(){}
+//TODO public void remove(){}
+//TODO public void clear(){}
+/**
+ *      PLEASE READ ME
+ *      ---------------
+ *
+ *      Added globalBroker to handle dataBase.
+ *      I assume here that the data is not persistant between app
+ *      iterations. If not, changes need to be made.
+ *
+ *      To be added:
+ *      populate table.
+ *      add elements.
+ *      remove elements.
+ *      clear elements.
+ *
+ * */

afcde07ae0cc17e3bd3d7438af16468d502a89bb
Multidream <luka.derado@gmail.com>
2017-10-17 13:47:13 -0400

    Java docs for ReportHolder.

--- app/src/main/java/pizzarat/cs2340/gatech/edu/structure/ReportHolder.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/structure/ReportHolder.java
@@ -4,6 +4,10 @@ package pizzarat.cs2340.gatech.edu.structure;
  * Created by Luka on 10/9/2017.
  */
 
+/**
+ *      This method holds local custom reports.
+ *      This prevents persistence, which we do not want for now.
+ */
 public class ReportHolder {
     public static ReportStructure data;
 }

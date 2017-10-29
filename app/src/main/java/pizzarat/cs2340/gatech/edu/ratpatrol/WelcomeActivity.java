01b16a5d478524750c9cf1c6e01622100708c683
Harrison Banh <harrisonbanh@gmail.com>
2017-10-28 22:15:39 -0400

    Made a funny splash

--- app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/WelcomeActivity.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/WelcomeActivity.java
@@ -2,6 +2,7 @@ package pizzarat.cs2340.gatech.edu.ratpatrol;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -9,6 +10,7 @@ import android.view.View;
  * A class representing the WelcomeActivity screen in the Rat Patrol app.
  */
 public class WelcomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     /**
      * Creates the welcome activity on startup
@@ -18,6 +20,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcomeSplashActivity = new Intent(WelcomeActivity.this,
+                        WelcomeSplashActivity.class);
+                startActivity(welcomeSplashActivity);
+            }
+
+        }, SPLASH_TIME_OUT);
     }
 
     /**

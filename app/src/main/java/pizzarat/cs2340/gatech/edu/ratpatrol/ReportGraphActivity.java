c8b8a439af0e83f8794d26b68afc203e7d0e5106
Harrison Banh <harrisonbanh@gmail.com>
2017-10-29 15:13:06 -0400

    Added nav to graph

--- app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/ReportGraphActivity.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/ReportGraphActivity.java
@@ -1,30 +1,34 @@
 package pizzarat.cs2340.gatech.edu.ratpatrol;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 import pizzarat.cs2340.gatech.edu.sqlite.SQLiteReportBroker;
-import pizzarat.cs2340.gatech.edu.structure.DateRangeStruct;
 import pizzarat.cs2340.gatech.edu.structure.GraphUtilities;
 import pizzarat.cs2340.gatech.edu.structure.ReportStructure;
 import pizzarat.cs2340.gatech.edu.structure.StaticHolder;
@@ -33,16 +37,32 @@ import pizzarat.cs2340.gatech.edu.structure.StaticHolder;
  * Represents the screen the graphically depicts all the rat reports based on
  * the user's desired filter.
  */
-public class ReportGraphActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, OnChartValueSelectedListener {
+public class ReportGraphActivity extends AppCompatActivity
+        implements SeekBar.OnSeekBarChangeListener, OnChartValueSelectedListener,
+        NavigationView.OnNavigationItemSelectedListener {
 
-    protected BarChart mChart;
     public SQLiteReportBroker reportBroker = new SQLiteReportBroker();
+    protected BarChart mChart;
     ArrayList<ReportStructure> reports;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)  {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_graph);
+
+        // Navigation drawer creation
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Report Creation");
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mChart = (BarChart) findViewById(R.id.ratChart);
         mChart.setOnChartValueSelectedListener(this);
@@ -98,6 +118,141 @@ public class ReportGraphActivity extends AppCompatActivity implements SeekBar.On
 
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_create_report) {
+            switchToCreateReportActivity();
+        } else if (id == R.id.nav_rat_archive) {
+            switchToArchiveActivity();
+        } else if (id == R.id.nav_filter) {
+            switchToFilterReportsScreen();
+        } else if (id == R.id.nav_sightings_map) {
+            switchToMapActivity();
+        } else if (id == R.id.nav_report_graphs) {
+            Toast message = Toast.makeText(getApplicationContext(),
+                    "You are already on this screen", Toast.LENGTH_LONG);
+            message.show();
+        } else if (id == R.id.nav_logout) {
+            logout();
+        } else if (id == R.id.nav_share) {
+            shareOrSendReport("Share");
+        } else if (id == R.id.nav_send) {
+            shareOrSendReport("Send");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * Switches to the WelcomeActivity from the Navigation Screen.
+     */
+    public void switchBackToWelcomeActivity() {
+        Intent switchToWelcomeActivity = new Intent(this, WelcomeActivity.class);
+        this.startActivity(switchToWelcomeActivity);
+    }
+
+    /**
+     * Switches to the ArchiveActivity from the Navigation Screen.
+     */
+    public void switchToArchiveActivity() {
+        Intent switchToArchiveActivity = new Intent(this, ArchiveActivity.class);
+        this.startActivity(switchToArchiveActivity);
+    }
+
+    /**
+     * Switches to the CreateReportActivity.
+     */
+    public void switchToCreateReportActivity() {
+        Intent switchToCreateReportActivity = new Intent(this, CreateReportActivity.class);
+        this.startActivity(switchToCreateReportActivity);
+    }
+
+    /**
+     * Switches to the MapActivity.
+     */
+    public void switchToMapActivity() {
+        Intent switchToMapActivity = new Intent(this, MapActivity.class);
+        this.startActivity(switchToMapActivity);
+    }
+
+    /**
+     * Switches to the FilterReportsActivity.
+     */
+    public void switchToFilterReportsScreen() {
+        Intent switchToFilterReportsActivity = new Intent(this, FilterReportsActivity.class);
+        this.startActivity(switchToFilterReportsActivity);
+    }
+
+    /**
+     * Closes the Navigation Screen thus "logging out" the user
+     */
+    public void logout() {
+        switchBackToWelcomeActivity();
+    }
+
+    /**
+     * Switches to the ReportGraphActivity.
+     */
+    public void switchToReportGraphScreen() {
+        Intent switchToReportGraphScreenActivity = new Intent(this, ReportGraphActivity.class);
+        startActivity(switchToReportGraphScreenActivity);
+        Toast.makeText(getBaseContext(), "To filter/edit graph, use the filter "
+                + "button on the Navigation Screen.", Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Opens a bunch of apps at the bottom of the screen allowing the user to
+     * share something about the Rat Patrol app using different apps.
+     *
+     * @param widget the name of widget clicked
+     */
+    public void shareOrSendReport(String widget) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+        String shareBody = "Your body here";
+        String shareSub = "Your subject here";
+        share.putExtra(Intent.EXTRA_SUBJECT, shareBody);
+        share.putExtra(Intent.EXTRA_TEXT, shareSub);
+        startActivity(Intent.createChooser(share, widget + " using"));
+
+    }
 
 
     private void setData(int count, float range) {
@@ -111,29 +266,22 @@ public class ReportGraphActivity extends AppCompatActivity implements SeekBar.On
         if (StaticHolder.dateRange != null)
             span = StaticHolder.dateRange.getDateSpan();
 
-        Log.d("hidden","span = " + span);
+        Log.d("hidden", "span = " + span);
         if (span >= 31536000) //bigger than year
         {
             getYearData(yVals1);
-        }
-        else if (span >= 2678400) //bigger than month
+        } else if (span >= 2678400) //bigger than month
         {
 
-        }
-        else
-        {
+        } else {
             //graph by day
             int[] days = GraphUtilities.organizeByMonth(reports);
-            for (int j = 0; j < days.length; j++)
-            {
+            for (int j = 0; j < days.length; j++) {
                 //yVals1.add(new BarEntry(j, j, getResources().getDrawable(R.drawable.graphmarker)));
                 yVals1.add(new BarEntry(j, days[j], getResources().getDrawable(R.drawable.graphmarker)));
             }
 
         }
-
-
-
 
 
         BarDataSet set1;
@@ -162,31 +310,26 @@ public class ReportGraphActivity extends AppCompatActivity implements SeekBar.On
         }
     }
 
-    public void getYearData(ArrayList<BarEntry> yVals)
-    {
-        Log.d("hidden","graphing by year");
+    public void getYearData(ArrayList<BarEntry> yVals) {
+        Log.d("hidden", "graphing by year");
         //graph by year
         int[] years = GraphUtilities.organizeByYear(getBaseContext(), reports);
-        for (int j = 0; j < years.length; j++)
-        {
+        for (int j = 0; j < years.length; j++) {
             //yVals1.add(new BarEntry(j, j, getResources().getDrawable(R.drawable.graphmarker)));
             yVals.add(new BarEntry(j, years[j], getResources().getDrawable(R.drawable.graphmarker)));
         }
     }
 
-    public void getMonthData(ArrayList<BarEntry> yVals)
-    {
+    public void getMonthData(ArrayList<BarEntry> yVals) {
         //graph by month
         int[] months = GraphUtilities.organizeByMonth(reports);
-        for (int j = 0; j < months.length; j++)
-        {
+        for (int j = 0; j < months.length; j++) {
             //yVals1.add(new BarEntry(j, j, getResources().getDrawable(R.drawable.graphmarker)));
             yVals.add(new BarEntry(j, months[j], getResources().getDrawable(R.drawable.graphmarker)));
         }
     }
 
-    public void getDayData()
-    {
+    public void getDayData() {
 
     }
 

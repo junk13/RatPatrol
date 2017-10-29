0cbe6ea13cce57b14b0a126243483e0b31c8452a
Multidream <luka.derado@gmail.com>
2017-10-29 05:04:03 -0400

    Added backend JavaDocs

--- app/src/main/java/pizzarat/cs2340/gatech/edu/structure/GraphUtilities.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/structure/GraphUtilities.java
@@ -7,7 +7,15 @@ import java.util.List;
  */
 
 public class GraphUtilities {
-    public int[] OrganizeByMonth(List<ReportStructure> reports){
+    /**
+     *      Organizes a List of Report Structures by their date entries
+     *      for each report.
+     *
+     * @param reports   All the reports one wants to compile into a int[]
+     * @return          An array of size 12, where each value represents
+     *                  the number of reports within the repective month.
+     */
+    public int[] organizeByMonth(List<ReportStructure> reports){
         int[] months = new int[12];
         for (ReportStructure report: reports) {
             String date = report.getDate();

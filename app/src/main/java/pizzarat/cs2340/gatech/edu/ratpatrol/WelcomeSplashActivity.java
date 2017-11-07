bb04e09b48a956c313a577da97a711fb5a10a659
Harrison Banh <harrisonbanh@gmail.com>
2017-11-06 19:16:02 -0500

    Reduced spelling warnings and deleted dead todos

--- app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/WelcomeSplashActivity.java
+++ app/src/main/java/pizzarat/cs2340/gatech/edu/ratpatrol/WelcomeSplashActivity.java
@@ -16,7 +16,7 @@ public class WelcomeSplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_splash);
 
-        // Start a thread to end the splash screen after a whilemn
+        // Start a thread to end the splash screen after a while
         Thread welcomeStartUp = new Thread() {
             @Override
             public void run() {
